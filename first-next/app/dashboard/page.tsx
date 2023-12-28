bc1f76cfd722e91527170aada22ce45dbb83b5ec
Vijay <vijayanand2k20@gmail.com>
2023-12-28 20:29:35 +0530

    Basics of Router

---
+++ first-next/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Dashboard",
+    description: "Dashboard page",
+}
+
+export default function DashboardPage() {
+    return (
+        <h1>Dashboard</h1>
+    )
+}
---
+++ first-next/app/example/page.tsx
@@ -0,0 +1,5 @@
+export default function example() {
+    return (
+        <h1>Example</h1>
+    )
+}
